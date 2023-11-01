269a9f78d58882334eac7a592ed38bf0ab6372bf
MichaelKorchagin <benderrodriguez932@Gmail.com>
2023-11-01 01:02:37 -0700

    Edit mobile layout

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const moreButton = document.getElementById("moreButton");
 const moreHidden = document.querySelector(".moreHidden");
 
 moreButton.addEventListener("click", function () {
-    if (moreHidden.style.visibility === "hidden") {
-        moreHidden.style.visibility = "visible";
+    if (moreHidden.style.display === "none") {
+        moreHidden.style.display = "flex";
     } else {
-        moreHidden.style.visibility = "hidden";
+        moreHidden.style.display = "none";
     }
 });
 
@@ -18,17 +18,49 @@ function popUpBannerCopy() {
     }, 2000);
 }
 
-
-
-
 const copyDiv = document.getElementById("copyDiv");
-copyDiv.addEventListener("click", function() {
+copyDiv.addEventListener("click", function () {
     const tempInput = document.createElement("input");
     tempInput.value = document.getElementById('copyNumber').innerText;
     document.body.appendChild(tempInput);
     tempInput.select();
     document.execCommand("copy");
     document.body.removeChild(tempInput);
-    
+
     popUpBannerCopy();
+});
+
+const openNavButton = document.getElementById("openNavButton");
+const nav2 = document.getElementById("nav2");
+openNavButton.addEventListener("click", function () {
+    if (nav2.style.bottom === "-1400px") {
+        nav2.style.bottom = "-500px";
+    } else {
+        nav2.style.bottom = "-1400px";
+    }
+
+    if (openNavButton.style.display === 'block') {
+        openNavButton.classList.add('openNavButtonArrow');
+        openNavButton.style.display = 'inline-block';
+    } else {
+        openNavButton.classList.remove('openNavButtonArrow');
+        openNavButton.style.display = 'block';
+    }
+});
+
+function visibilitySwipeButton() {
+    if (window.innerWidth > 1292) {
+        document.getElementById('openNavButton').style.visibility = 'hidden';
+    } else {
+        document.getElementById('openNavButton').style.visibility = 'visible';
+    }
+}
+
+
+
+window.addEventListener('load', () => {
+    visibilitySwipeButton();
+});
+window.addEventListener('resize', () => {
+    visibilitySwipeButton();
 });
